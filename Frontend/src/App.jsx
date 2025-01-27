d5cde52577b04ce8c2f07505b1eaf8723d8729f5
Prarabdh Soni <pscs.prarabdhsoni2005@gmail.com>
2025-01-27 17:56:11 +0530

    frontend basic structure

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './Pages/Home/Index';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Navigate to="/" />} />
+        
+      </Routes> 
+    </Router>
+  );
+}
+
+export default App;
