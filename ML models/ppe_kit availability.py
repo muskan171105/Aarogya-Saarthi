3024751b07aa7354314175307d38a8b23c04c8f9
Shorya783 <pathakshorya94@gmail.com>
2025-02-02 01:07:58 +0530

    Update ppe_kit availability.py

--- ML models/ppe_kit availability.py
+++ ML models/ppe_kit availability.py
@@ -1,5 +1,8 @@
+# importing the dependencies 
+
 import numpy as np
 import pandas as pd
+import os
 import random
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -7,49 +10,55 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
-seed = 42
-np.random.seed(seed)
-random.seed(seed)
-tf.random.set_seed(seed)
-tf.config.experimental.enable_op_determinism()
-df=pd.read_csv('/content/PPE_Kit_Availability_Dataset.csv')
-df=df.drop(columns=['Hospital_Type'])
-df=df.drop(columns=['Number_of_Patients'])
-df=df.drop(columns=['Avg_Patient_Load_Last_Month'])
-df=df.drop(columns=['Hospital_Location'])
-df=df.drop(columns=['Budget_Allocation_for_PPE'])
-X=df.drop(columns=['PPE_Kits_Available'])
-y=df['PPE_Kits_Available']
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=seed)
+
+#Loading the dataset
+
+df=pd.read_csv('/content/Updated_PPE_Kit_Availability_Dataset.csv')
+
+#removing the columns which containing missing values 
+df=df.dropna(axis=1,how='all')
+
+X=df.drop(columns=['PPE_Kits_Available_in_october','PPE Kits Available in November','PPE Kits Available in December','PPEKAIJ'])
+y=df[['PPE_Kits_Available_in_october','PPE Kits Available in November','PPE Kits Available in December','PPEKAIJ']]
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=2)
+
 model=LinearRegression()
+
 model.fit(X_train,y_train)
+
 training_data_prediction=model.predict(X_train)
 training_data_accuacy=r2_score(y_train,training_data_prediction)
-#print(training_data_accuacy)
-testin_data_prediction=model.predict(X_test)
-testin_data_accuacy=r2_score(y_test,testin_data_prediction)
-#print(testin_data_accuacy)
-y_preds=model.predict(X_test)
-#plt.scatter(X_train.iloc[:,0],y_train,c='b',label='training data')
-#plt.scatter(X_test.iloc[:,0],y_test,c='g',label='testing data')
-#plt.scatter(X_test.iloc[:,0],y_preds,c='r',label='predicted values')
-#plt.legend()
-#plt.show()
-model=tf.keras.Sequential({
-    tf.keras.layers.Dense(100,activation='relu')
-    ,tf.keras.layers.Dense(100,activation='relu')
-    ,tf.keras.layers.Dense(100,activation='relu')
-    ,tf.keras.layers.Dense(1,activation='relu') 
-})
-model.compile(loss=tf.keras.losses.mae,
-              optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-              metrics=['mae'])
-history=model.fit(X_train,y_train,epochs=100,verbose=0,shuffle=False)
-model.evaluate(X_test,y_test)
-pd.DataFrame(history.history).plot()
-plt.ylabel('loss')
-plt.xlabel('epochs')
+print('Accuracy on training data:',training_data_accuacy)
+
+testing_data_prediction=model.predict(X_test)
+testing_data_accuacy=r2_score(y_test,testing_data_prediction)
+print('Accuracy on testing data:',testing_data_accuacy)
+
+#Backend code added here 
+
+input_data=([250,250,110])
+input_data_as_numpy_array=np.asarray(input_data)
+input_data_reshaped=input_data_as_numpy_array.reshape(1,-1)
+prediction=model.predict(input_data_reshaped)
+print('Number of PPE kits available in march',abs(int(prediction[0][0])))
+print('Number of PPE kits available in april',abs(int(prediction[0][1])))
+print('Number of PPE kits available in may',abs(int(prediction[0][2])))
+
+# Sample data for demonstration
+
+data = [200,100,110,115,int(prediction[0][0]),int(prediction[0][1]),int(prediction[0][2])]
+months = ['oct','nov','dec','jan','feb','mar','apr','may']
+
+plt.figure(figsize=(10, 5))
+plt.plot(data, marker='o', linestyle='-', color='blue')
+
+# Add month names on the x-axis
+
+plt.xticks(ticks=range(len(months)), labels=months)
+
+plt.title("PPE kits Plot")
+plt.xlabel("Months")
+plt.ylabel("PPE kits")
+plt.grid(True)
 plt.show()
-test_loss,test_mae=model.evaluate(X_test,y_test)
-print(test_loss)
-print(test_mae)
