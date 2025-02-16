e0bfe242956aa85fecd84dd02197c9505c2e4d69
muskan171105 <muskanm1711@gmail.com>
2025-02-16 12:29:25 +0530

    Bed Rendered

--- ML models/ppe_testing/app.py
+++ ML models/ppe_testing/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import joblib
 from pymongo import MongoClient
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for cross-origin requests
@@ -71,5 +72,10 @@ def predict_ppe():
     
     return jsonify(response)
 
+@app.route('/')
+def home():
+    return "Hello, Render!"
+
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000)) 
     app.run(debug=True)
