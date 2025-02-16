036ff76f921f36ffc69458087b100d9e86d96232
muskan171105 <muskanm1711@gmail.com>
2025-02-16 12:42:54 +0530

    Bed Rendered attempt 4

--- ML models/ppe_testing/app.py
+++ ML models/ppe_testing/app.py
@@ -77,5 +77,5 @@ def home():
     return "Hello, Render!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000)) 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
