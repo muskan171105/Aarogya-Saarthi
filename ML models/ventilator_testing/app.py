ac966cc297e9fdde24703412f8d62e62e6da2b7b
muskan171105 <muskanm1711@gmail.com>
2025-02-16 14:43:14 +0530

    Ventilator Render Attempt 1

--- ML models/ventilator_testing/app.py
+++ ML models/ventilator_testing/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
@@ -36,5 +37,10 @@ def predict():
     else:
         return jsonify({"message": "No records found"}), 404
 
+@app.route('/')
+def home():
+    return "Ventilator API Running!"
+
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    port = int(os.environ.get("PORT", 10000)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
