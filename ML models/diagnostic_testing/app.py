bf302c72f019965bab2af1d85730746857dbefb3
Pragalbh Sharma <pragalbh.katyayan@gmail.com>
2025-02-18 00:55:46 +0530

    Adding Fixation on Dignostic

--- ML models/diagnostic_testing/app.py
+++ ML models/diagnostic_testing/app.py
@@ -64,5 +64,5 @@ def home():
     return "Diagnostic API is running!"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000)) 
+    port = int(os.environ.get("PORT", 11000)) 
     app.run(host="0.0.0.0", port=port, debug=True)
